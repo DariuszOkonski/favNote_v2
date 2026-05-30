a6c1836c303c98c968760bff9f288b27ed11b4cf
Dariusz Okoński <darek200180@gmail.com>
2026-05-30 06:45:15 +0200

    add basic button

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components';
+import Button from 'components/Button/Button';
 
 // NODE - v14
-
 const Root = () => {
-  return <h1>FavNotes</h1>;
+  return (
+    <div>
+      <h1>Hello Roman</h1>
+      <Button>My button</Button>
+    </div>
+  );
 };
 
 export default Root;
