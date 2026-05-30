81ae097b4df77a585291cd1b665114b7d7356921
Dariusz Okoński <darek200180@gmail.com>
2026-05-30 06:24:52 +0200

    install styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import styled from 'styled-components';
 
 // NODE - v14
 // npm install styled-components@5
+
+const MyButton = styled.button`
+  border: 2px solid blue;
+  padding: 50px;
+`;
+
 const App = () => {
   return (
     <div className='App'>
