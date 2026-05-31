cc71fddd4e470f5618566ef0bf15e14b6b07de45
Dariusz Okoński <darek200180@gmail.com>
2026-05-31 07:13:04 +0200

    add color props to Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: #ffd82b;
+  background-color: ${({ color }) => color || '#ffd82b'};
   width: ${({ width }) => width || '220px'};
   height: 47px;
   border: none;
