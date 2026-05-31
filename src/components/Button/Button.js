eebc237e967f5f292209d91fccb22b1c7bd72b06
Dariusz Okoński <darek200180@gmail.com>
2026-05-31 07:20:04 +0200

    add colors to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,11 +4,12 @@ export const buttonBackgroundOptions = {
   primary: 'hsl(49, 100%, 58%)',
   secondary: 'hsl(196, 83%, 75%)',
   tertiary: 'hsl(106, 47%, 64%)',
+  gray: 'hsl(0, 0%, 90%)',
 };
 
 const Button = styled.button`
   padding: 0;
-  background-color: ${({ color }) => color || '#ffd82b'};
+  background-color: ${({ color }) => color || buttonBackgroundOptions.primary};
   width: ${({ width }) => width || '220px'};
   height: 47px;
   border: none;
@@ -21,7 +22,7 @@ const Button = styled.button`
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: #e6e6e6;
+      background-color: ${buttonBackgroundOptions.gray};
       width: 105px;
       height: 30px;
       font-size: 10px;
