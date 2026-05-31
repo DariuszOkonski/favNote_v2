e2d042587e24614378c16cb539bfce94b0892a4a
Dariusz Okoński <darek200180@gmail.com>
2026-05-31 06:43:40 +0200

    use node in version 18

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,13 +1,13 @@
 import Button from 'components/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
 
-// NODE - v14
+// NODE - v18
 const Root = () => {
   return (
     <div>
       <GlobalStyle />
       <h1>Hello Roman</h1>
-      <Button width='500px'>Close / Save</Button>
+      <Button width='300px'>Close / Save</Button>
       <Button>Close / Save</Button>
       <Button secondary>Remove</Button>
     </div>
