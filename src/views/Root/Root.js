4df7f1cc2fa9deac7c1e2bb886597b29a3f31a2d
Dariusz Okoński <darek200180@gmail.com>
2026-05-31 06:35:23 +0200

    add GlobalStyle component

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,9 +1,11 @@
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 // NODE - v14
 const Root = () => {
   return (
     <div>
+      <GlobalStyle />
       <h1>Hello Roman</h1>
       <Button width='500px'>Close / Save</Button>
       <Button>Close / Save</Button>
