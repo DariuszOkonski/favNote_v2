575e841546dd0a4dc494c73b9072246fb5a5c067
Dariusz Okoński <darek200180@gmail.com>
2026-06-02 06:20:36 +0200

    add Heading, Input and Paragraph components

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -3,6 +3,9 @@ import Button from 'components/atoms/Button/Button';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'theme/GlobalStyle';
 import { theme } from 'theme/mainTheme';
+import Heading from 'components/atoms/Heading/Heading';
+import Input from 'components/atoms/Input/Input';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
 
 // NODE - v20.19.0
 const Root = () => {
@@ -17,6 +20,10 @@ const Root = () => {
           </Button>
           <Button>Close / Save</Button>
           <Button secondary>Remove</Button>
+
+          <Heading>Heading h1</Heading>
+          <Input placeholder='Input' />
+          <Paragraph>Paragraph</Paragraph>
         </Fragment>
       </ThemeProvider>
     </div>
